3a126dc74eca05dcb26006c3bfc2fb58faab27b6
Brandon Manuel <brandon-manuel@live.com>
2018-12-15 23:37:55 -0500

    Removed success and failure properties on success and failure responses since it was redundant

--- server/src/helpers/responseHelper.js
+++ server/src/helpers/responseHelper.js
@@ -1,9 +1,9 @@
 module.exports = {
   sendSuccess: function(res, success) {
-    res.status(200).json({ success });
+    res.status(200).json(success);
   },
 
   sendFailure: function(res, failure) {
-    res.status(500).json({ failure });
+    res.status(500).json(failure);
   }
 };
