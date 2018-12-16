3ec1f511311b896b20fc49fd0c47cc69f3178fe4
Brandon Manuel <brandon-manuel@live.com>
2018-12-15 23:38:52 -0500

    Added lambdas on spotify api access token initialization

--- server/src/index.js
+++ server/src/index.js
@@ -15,14 +15,14 @@ var spotifyApi = new SpotifyWebApi({
 });
 
 spotifyApi.clientCredentialsGrant().then(
-  function(data) {
+  data => {
     console.log('The access token expires in ' + data.body['expires_in']);
     console.log('The access token is ' + data.body['access_token']);
 
     // Save the access token so that it's used in future calls
     spotifyApi.setAccessToken(data.body['access_token']);
   },
-  function(err) {
+  err => {
     console.log(
       'Something went wrong when retrieving an access token',
       err.message
