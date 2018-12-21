08649adeed92d57dc26b049e34ac3cd78f8ace54
BrodyJack <brodyjackj@gmail.com>
2018-12-21 08:52:25 -0600

    removed info from Food that is now in Info, added some structure to chat component

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Chat = (props) => (
-    <div>
-      <span>Chat</span>
-    </div>
-);
+export default class Chat extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: []
+        }
+    }
 
-export default Chat;
+    render() {
+        const { currentRoom } = this.props;
+
+        let noRoom = currentRoom === ""
+            ? (
+                <div style={ styles.outerdiv }>
+                    <p>Join a room to begin chatting!</p>
+                </div>
+            ) : (
+                <div style= { styles.outerdiv }>
+                    <p>You are chatting in room: {currentRoom}</p>
+                </div>
+            )
+
+        return noRoom;
+    }
+}
+
+let styles = {
+    outerdiv: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignContent: 'center',
+        flexDirection: 'column',
+        minHeight: '80vh'
+    }
+}
